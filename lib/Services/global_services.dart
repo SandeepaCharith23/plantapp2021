2eaf04a8f5ae37c2dcb93404f9e0050da42030b5
Sandeepa charith <sandeepacharith23@gmail.com>
2023-01-25 12:07:55 +0530

    2023-January -Updated

---
+++ lib/Services/global_services.dart
@@ -0,0 +1,10 @@
+String loginUrlImage =
+    'https://images.unsplash.com/photo-1518531933037-91b2f5f229cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80';
+String loginUrlImage1 =
+    'https://images.unsplash.com/photo-1570445997127-459affe1cc25?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1854&q=80';
+String loginUrlImage2 =
+    'https://images.unsplash.com/photo-1669154413133-19258726d333?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1936&q=80';
+String loginUrlImage3 =
+    'https://images.unsplash.com/photo-1669090787997-1d7878a44be7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80';
+String loginUrlImage4 =
+    'https://images.unsplash.com/photo-1522204523234-8729aa6e3d5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80';
--- lib/screens/Page1-WelcomeScreen/welcomescreen.dart
+++ lib/screens/Page1-WelcomeScreen/welcomescreen.dart
@@ -1,5 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:plantapp2021/Services/global_services.dart';
 import 'package:plantapp2021/screens/Common_Components/RoundedButton.dart';
 import 'package:plantapp2021/screens/Page2-LoginScreen/SignInScreen.dart';
 
@@ -13,14 +15,13 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
   AnimationController? animationController;
+  late Animation<double> _animation1;
+  late AnimationController _animationController1;
 
   @override
   void initState() {
-    // TODO: implement initState
-    super.initState();
-
     animationController = AnimationController(
       vsync: this,
       duration: Duration(seconds: 2),
@@ -31,12 +32,32 @@ class _WelcomeScreenState extends State<WelcomeScreen>
       setState(() {});
       // print(animationController!.value);
     });
+
+    _animationController1 =
+        AnimationController(vsync: this, duration: const Duration(seconds: 50));
+
+    _animation1 =
+        CurvedAnimation(parent: _animationController1, curve: Curves.linear)
+          ..addListener(() {
+            setState(() {});
+          })
+          ..addStatusListener((animationStatus) {
+            if (animationStatus == AnimationStatus.completed) {
+              _animationController1.reset();
+              _animationController1.forward();
+            }
+          });
+    _animationController1.forward();
+
+    // TODO: implement initState
+    super.initState();
   }
 
   @override
   void dispose() {
     animationController!.dispose();
     // TODO: implement dispose
+    _animationController1.dispose();
     super.dispose();
   }
 
@@ -44,96 +65,106 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       //backgroundColor: Colors.deepOrange,
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/welcomescreenbackground.jpg"),
-            fit: BoxFit.fill,
+      body: Stack(
+        children: [
+          CachedNetworkImage(
+            imageUrl: loginUrlImage4,
+            placeholder: (context, url) => Image.asset(
+              'assets/images/welcomescreenbackground.jpg',
+              fit: BoxFit.fill,
+            ),
+            fit: BoxFit.cover,
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+            width: double.infinity,
+            height: double.infinity,
+            alignment: FractionalOffset(_animation1.value, 0),
           ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(5.0),
-                child: Row(
-                  children: <Widget>[
-                    Hero(
-                      tag: 'Logotag',
-                      child: Container(
-                        child: Image.asset('assets/logos/logo.png'),
-                        width: 40.0,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: Row(
+                    children: <Widget>[
+                      Hero(
+                        tag: 'Logotag',
+                        child: Container(
+                          child: Image.asset('assets/logos/logo.png'),
+                          width: 40.0,
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 20.0,
-                    ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
 
-                    DefaultTextStyle(
-                      style: const TextStyle(
-                        fontSize: 20.0,
-                        fontFamily: 'Agne',
-                      ),
-                      child: AnimatedTextKit(
-                        animatedTexts: [
-                          TypewriterAnimatedText(
-                            'Plant App-2022-විසිතුරු පැළෑටි සහ මත්ස්‍යයින්',
-                            textAlign: TextAlign.center,
-                            textStyle: TextStyle(
-                              fontSize: 12,
+                      DefaultTextStyle(
+                        style: const TextStyle(
+                          fontSize: 20.0,
+                          fontFamily: 'Agne',
+                        ),
+                        child: AnimatedTextKit(
+                          animatedTexts: [
+                            TypewriterAnimatedText(
+                              'PlantApp-2022-විසිතුරු පැළෑටි සහ මත්ස්‍යයින්',
+                              textAlign: TextAlign.center,
+                              textStyle: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
                             ),
-                          ),
-                        ],
-                        onTap: () {
-                          print("Tap Event");
-                        },
+                          ],
+                          onTap: () {
+                            print("Tap Event");
+                          },
+                        ),
                       ),
-                    ),
-                    // Text(
-                    //   'Endemic Species -Srilanka',
-                    //   style: TextStyle(
-                    //     fontSize: 20,
-                    //     fontWeight: FontWeight.bold,
-                    //   ),
-                    //   ),
-                  ],
+                      // Text(
+                      //   'Endemic Species -Srilanka',
+                      //   style: TextStyle(
+                      //     fontSize: 20,
+                      //     fontWeight: FontWeight.bold,
+                      //   ),
+                      //   ),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              RoundedButton(
-                colour: Colors.lightBlueAccent,
-                text: 'Login-පිවිසෙන්න',
-                method: () {
-                  // Navigator.pushNamed(context, LoginScreen.id);
-                  Navigator.pushNamed(context, SignInScreen.id);
-                },
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 10.0),
-                child: Material(
-                  elevation: 5.0,
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.deepOrangeAccent,
-                  child: MaterialButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, RegistrationPage.id);
-                    },
-                    minWidth: 200.0,
-                    height: 42.0,
-                    child: Text(
-                      'Register-ගිණුමක් ආරම්භ ',
+                SizedBox(
+                  height: 30.0,
+                ),
+                RoundedButton(
+                  colour: Colors.lightBlueAccent,
+                  text: 'Login-පිවිසෙන්න',
+                  method: () {
+                    // Navigator.pushNamed(context, LoginScreen.id);
+                    Navigator.pushNamed(context, SignInScreen.id);
+                  },
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.deepOrangeAccent,
+                    child: MaterialButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, RegistrationPage.id);
+                      },
+                      minWidth: 200.0,
+                      height: 42.0,
+                      child: Text(
+                        'Register-ගිණුමක් ආරම්භ ',
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
